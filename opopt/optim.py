c2a379905be9cf7a3d744a86e6d37c31997d3ff7
ekgren <ariel.ekgren@gmail.com>
2021-10-17 21:30:50 +0200

    Added ClampSGD

---
+++ opopt/optim.py
@@ -0,0 +1,42 @@
+import torch
+from torch.optim import Optimizer
+
+
+class ClampSGD(Optimizer):
+    r"""Clamp SGD
+
+    Optimizer that clamps and divide grad by clamp param.
+
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-1)
+        clamp (float, optional): clamping of gradient (default: 1e-30)
+        drop (float, optional): dropout of gradient (default: 0)
+    """
+
+    def __init__(self, params, lr=1e-1, clamp=1e-30, drop=0.0):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= drop < 1.0:
+            raise ValueError("Invalid dropout value: {}".format(drop))
+        defaults = dict(lr=lr, clamp=clamp, drop=drop)
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                lr = group["lr"]
+                clamp = group["clamp"]
+                drop = group["drop"]
+                grad = torch.nn.functional.dropout(
+                    p.grad.data.clamp_(-clamp, clamp).div_(clamp), p=drop
+                )
+                p.data.add_(grad, alpha=-lr)
+        return loss
